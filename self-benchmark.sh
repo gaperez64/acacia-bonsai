1f9ed497c82666c600cd77a8ecee970da2684186
Michaël Cadilhac <michael@cadilhac.name>
2022-01-21 11:44:01 -0600

    Fix typos.

--- self-benchmark.sh
+++ self-benchmark.sh
@@ -6,9 +6,31 @@ opt='-march=native -O3 -flto -fuse-linker-plugin -pipe -DNO_VERBOSE'
 
 declare -A confs
 
+defaults=$(<<EOF 
+-DDEFAULT_K='11'
+-DDEFAULT_KMIN='-1u'
+-DDEFAULT_KINC='0'
+-DDEFAULT_UNREAL_X='UNREAL_X_BOTH'
+-DVECTOR_ELT_T='char'
+-DK_BOUNDED_SAFETY_AUT_IMPL='k_bounded_safety_aut'
+-DSTATIC_ARRAY_MAX='300'
+-DSTATIC_MAX_BITSETS='3ul'
+-DARRAY_IMPL='simd_array_backed_sum'
+-DVECTOR_IMPL='simd_vector_backed'
+-DARRAY_AND_BITSET_DOWNSET_IMPL='vector_backed_bin'
+-DVECTOR_AND_BITSET_DOWNSET_IMPL='vector_backed_bin'
+-DSIMD_IS_MAX='true'
+-DAUT_PREPROCESSOR='aut_preprocessors::surely_losing'
+-DBOOLEAN_STATES='boolean_states::forward_saturation'
+-DIOS_PRECOMPUTER='ios_precomputers::standard'
+-DACTIONER='actioners::standard<typename SetOfStates::value_type>'
+-DINPUT_PICKER='input_pickers::critical_pq'
+EOF
+        )
+
 # These all differ from the base configuration by /one/ option.
 confs=(
-    [base]="-DDEFAULT_K='11' -DDEFAULT_KMIN='-1u' -DDEFAULT_KINC='0' -DDEFAULT_UNREAL_X='UNREAL_X_BOTH' -DVECTOR_ELT_T='char' -DK_BOUNDED_SAFETY_AUT_IMPL='k_bounded_safety_aut' -DSTATIC_ARRAY_MAX='300' -DSTATIC_MAX_BITSETS='3ul' -DARRAY_IMPL='simd_array_backed_sum' -DVECTOR_IMPL='simd_vector_backed' -DARRAY_AND_BITSET_DOWNSET_IMPL='vector_backed_bin' -DVECTOR_AND_BITSET_DOWNSET_IMPL='vector_backed_bin' -DSIMD_IS_MAX='true' -DAUT_PREPROCESSOR='aut_preprocessors::surely_losing' -DBOOLEAN_STATES='boolean_states::forward_saturation' -DIOS_PRECOMPUTER='ios_precomputers::standard' -DACTIONER='actioners::standard<typename SetOfStates::value_type>' -DINPUT_PICKER='input_pickers::critical_pq'"
+    [base]=" "
     [kmin5_kinc2]="-DDEFAULT_KMIN=5 -DDEFAULT_KINC=2"
     [kmin5_kinc1]="-DDEFAULT_KMIN=5 -DDEFAULT_KINC=1"
     [kmin2_kinc1]="-DDEFAULT_KMIN=2 -DDEFAULT_KINC=1"
@@ -20,7 +42,7 @@ confs=(
     [autpreproc_standard]="-DAUT_PREPROCESSOR=aut_preprocessors::standard"
     [autpreproc_nopreproc]="-DAUT_PREPROCESSOR=aut_preprocessors::no_preprocessing"
     [booleanstates_none]="-DBOOLEAN_STATES=boolean_states::no_boolean_states"
-    [iosprecom_delegate]="-DIOS_PRECOMPUTER=ios_precomputers::delegate -DACTIONERS='actioners::no_ios_precomputation<typename SetOfStates::value_type>'"
+    [iosprecom_delegate]="-DIOS_PRECOMPUTER=ios_precomputers::delegate -DACTIONER='actioners::no_ios_precomputation<typename SetOfStates::value_type>'"
     [iosprecom_fake_vars]="-DIOS_PRECOMPUTER=ios_precomputers::fake_vars"
     [iosprecom_powset]="-DIOS_PRECOMPUTER=ios_precomputers::powset"
     [inputpicker_critical]="-DINPUT_PICKER=input_pickers::critical"
@@ -31,12 +53,18 @@ confs=(
 for name param in ${(kv)confs}; do
     build=build_$name
     log=_bm-logs/$name.log
+    rm -f $log
     if [[ -e $build ]]; then
         echo "$build exists, not rebuilding."
     else
-        echo -n "building $build"
-        CXXFLAGS="$opt $param" meson $build --buildtype=release &>> $log
-        echo " done."
+        echo -n "building $build... "
+        if CXXFLAGS="$opt $defaults $param" meson $build --buildtype=release &>> $log; then
+            echo "done."
+        else
+            echo "FAILED; exciting readily, please remove $build."
+            cat $log
+            exit 2
+        fi
     fi
 done
 
@@ -48,8 +76,8 @@ for name in ${(k)confs}; do
       continue
     fi
     cd $build
-    echo -n "compiling $name "
-    if meson compile &> ../$log; then
+    echo -n "compiling $name... "
+    if meson compile &>> ../$log; then
       echo "done"
       touch compiled
     else
@@ -70,7 +98,7 @@ for name in ${(k)confs}; do
         continue
     fi
     cd $build
-    echo -n "benchmarking $name"
+    echo -n "benchmarking $name... "
     meson test --benchmark --suite=ab/syntcomp21/crit -t 1.7 &>> ../$log
     echo "done"
     touch benchmarked
