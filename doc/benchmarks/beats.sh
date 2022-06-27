58250e5ea6f2c0ce04baa9b595b73e763a650912
Michaël Cadilhac <michael@cadilhac.name>
2022-06-27 16:49:15 -0500

    Allow scientific notation and indicating timeout.

--- doc/benchmarks/beats.sh
+++ doc/benchmarks/beats.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
-(( $# == 2 )) || { echo "usage: $0 file1.json file2.json"; exit 2 }
+(( $# == 2 || $# == 3 )) || { echo "usage: $0 file1.json file2.json [timeoutvalue]"; exit 2 }
 
 tobeat=$1
 fighter=$2
+timeout=${3:-17}
 
 while read test; do
   case $test; in
@@ -10,13 +11,13 @@ while read test; do
     *) continue ;;
   esac
   name=$(sed 's@[^/]*/\([^"]*\).*@\1@' <<< $test)
-  timetobeat=$(sed 's/.*"rtime": \([0-9.]*\) .*/\1/' <<< $test)
-  fightertime=$(grep -F "/$name\"" $fighter | grep '"status": true' | sed 's/.*"rtime": \([0-9.]*\) .*/\1/')
+  timetobeat=$(sed 's/.*"rtime": \([0-9.e-]*\) .*/\1/' <<< $test)
+  fightertime=$(grep -F "/$name\"" $fighter | grep '"status": true' | sed 's/.*"rtime": \([0-9.e-]*\) .*/\1/')
   [[ -z $fightertime ]] && continue
   case $test; in
-    *'"status": false'*) echo "$name (TO vs $fightertime)";;
+    *'"status": false'*) echo "$name (TO vs $fightertime, at least diff $(printf '%08f' $((timeout - fightertime))))";;
     *)  if (( fightertime < timetobeat )); then
-          echo "$name ($timetobeat vs $fightertime, diff $((timetobeat - fightertime)))"
+          echo "$name ($timetobeat vs $fightertime, diff $(printf '%08f' $((timetobeat - fightertime))))"
         fi;;
   esac
 done < $tobeat
