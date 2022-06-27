28d721afdf3b6534b3a521f712d180038ce76d60
Michaël Cadilhac <michael@cadilhac.name>
2022-06-27 16:48:20 -0500

    Optionally disregard spot.

--- doc/benchmarks/meson-to-mkplot.sh
+++ doc/benchmarks/meson-to-mkplot.sh
@@ -1,13 +1,38 @@
 #!/bin/zsh -f
 
-(( $# == 2 )) || { echo "usage: $0 PROGNAME MESON-TEST.JSON"; exit 2 }
+PROG=$0
+
+usage () {
+  cat <<EOF
+Usage: $0 [-d] PROGNAME MESON-TEST.JSON
+  -d: Disregard Spot computational time.
+EOF
+  exit 1
+}
+
+(( $# == 2 || $# == 3 )) || usage
+
+disregard_spot=false
+if (( $# == 3 )); then
+  case $1; in
+    '-d') disregard_spot=true;;
+    *) usage;;
+  esac
+  shift
+fi
 
 cat <<EOF
 {
   "stats": {  
 EOF
 
-perl -pe 's@.*? / (.*?)".*"result": (.*?),.*"duration": (.*?),.*@"\1": { "status": \2, "rtime": \3 },@;s/"OK"/true/;s@"(KO|TIMEOUT)"@false@' $2
+if $disregard_spot; then
+  grep 'Time disregarding' $2 | \
+     perl -pe 's@.*? / (.*?)".*Time disregarding Spot translation: (.*?) seconds.*"result": (.*?),.*@"\1": { "status": \3, "rtime": \2 },@;s/"OK"/true/;s@"(KO|TIMEOUT)"@false@'
+else
+  perl -pe 's@.*? / (.*?)".*"result": (.*?),.*"duration": (.*?),.*@"\1": { "status": \2, "rtime": \3 },@;s/"OK"/true/;s@"(KO|TIMEOUT)"@false@' $2
+fi
+
 
 cat <<EOF
    "result": { "status": false, "rtime": 0 }
