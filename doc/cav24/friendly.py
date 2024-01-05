bfd45c5b91a18bdf172e1d4332791d7f39fe68e0
Guillermo A. Perez <gaperez64@gmail.com>
2024-01-05 16:05:56 +0100

    all experiments and graphs almost automated, run scripts to reproduce

---
+++ doc/cav24/friendly.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import json
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+def loadTests(fname):
+    print(fname)
+    with open(fname, 'r') as f:
+        stats = json.load(f)["stats"]
+    bench = []
+    rtime = []
+    del stats["result"]
+    for exp in stats:
+        bench.append(exp[exp.find("/") + 1:exp.rfind(".ltl")])
+        rtime.append(float(stats[exp]["rtime"]))
+    return (bench, rtime)
+
+
+def graph(results):
+    for k in results:
+        (bench, rtime) = results[k]
+        plt.scatter(bench, rtime, label=k)
+    # plt.yscale("log")
+    plt.xlabel("Benchmarks")
+    plt.ylabel("Running time (s)")
+    plt.xticks(rotation=30)
+    # show plots
+    plt.legend(loc="lower left")
+    plt.show()
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1
+    results = dict()
+    for file in os.listdir(sys.argv[1]):
+        if file.endswith(".json2"):
+            (bench, rtime) = loadTests(os.path.join(sys.argv[1], file))
+            results[file] = (bench, rtime)
+    graph(results)
+    exit(os.EX_OK)
