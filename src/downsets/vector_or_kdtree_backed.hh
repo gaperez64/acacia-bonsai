f7684bfb4d06a875dce6c9ab3543d192119e4304
micha <michael@cadilhac.name>
2024-01-05 08:38:44 -0500

    Sanitize pointer usage, use pool for kdtree.

--- src/downsets/vector_or_kdtree_backed.hh
+++ src/downsets/vector_or_kdtree_backed.hh
@@ -32,8 +32,8 @@ namespace downsets {
   template <typename Vector>
   class vector_or_kdtree_backed {
     private:
-      std::shared_ptr<vector_backed<Vector>> vector;
-      std::shared_ptr<kdtree_backed<Vector>> kdtree;
+      std::unique_ptr<vector_backed<Vector>> vector;
+      std::unique_ptr<kdtree_backed<Vector>> kdtree;
 
       template <typename V>
       friend std::ostream& operator<<(std::ostream& os, const vector_or_kdtree_backed<V>& f);
@@ -43,7 +43,7 @@ namespace downsets {
           return this->vector->vector_set.size ();
         else {
           assert (this->kdtree != nullptr);
-          return this->kdtree->tree->vector_set.size ();
+          return this->kdtree->tree.vector_set.size ();
         }
       }
 
@@ -57,23 +57,23 @@ namespace downsets {
           if (inter)
             this->vector->intersect_with (std::move (*(other.vector)));
           else
-            this->vector->union_with (std::move (*(other.vector))); 
+            this->vector->union_with (std::move (*(other.vector)));
         } else if (this->kdtree == nullptr && other.kdtree != nullptr) {
           // case 2. reinterpret kdtree as vector
           assert (this->vector != nullptr);
           assert (other.vector == nullptr);
           // this costs linear time already: reinterpret the kdtree as a
           // vector
-          vector_backed<Vector> B = vector_backed<Vector> (std::move (other.kdtree->tree->vector_set));
+          vector_backed<Vector> B = vector_backed<Vector> (std::move (other.kdtree->tree.vector_set));
           if (inter)
             this->vector->intersect_with (std::move (B));
           else
-            this->vector->union_with (std::move (B)); 
+            this->vector->union_with (std::move (B));
         } else if (other.kdtree == nullptr && this->kdtree != nullptr) {
           // case 3. again reinterpret kdtree as vector
           assert (other.vector != nullptr);
           assert (this->vector == nullptr);
-          this->vector = std::make_shared<vector_backed<Vector>> (std::move (this->kdtree->tree->vector_set));
+          this->vector = std::make_unique<vector_backed<Vector>> (std::move (this->kdtree->tree.vector_set));
           this->kdtree = nullptr;
           if (inter)
             this->vector->intersect_with (std::move (*(other.vector)));
@@ -97,9 +97,9 @@ namespace downsets {
             else
               this->kdtree->union_with (std::move (*(other.kdtree)));
           } else {
-            this->vector = std::make_shared<vector_backed<Vector>> (std::move (this->kdtree->tree->vector_set));
+            this->vector = std::make_unique<vector_backed<Vector>> (std::move (this->kdtree->tree.vector_set));
             this->kdtree = nullptr;
-            vector_backed<Vector> B = vector_backed<Vector> (std::move (other.kdtree->tree->vector_set));
+            vector_backed<Vector> B = vector_backed<Vector> (std::move (other.kdtree->tree.vector_set));
             if (inter)
               this->vector->intersect_with (std::move (B));
             else
@@ -114,8 +114,8 @@ namespace downsets {
         size_t dim = this->dim ();
         data_do (std::cout << "|VEKD: downset_size="
                          << dim << "," << m << "|" << std::endl);
-        if (this->kdtree == nullptr && KD_THRESH(m, dim)) { 
-          this->kdtree = std::make_shared<kdtree_backed<Vector>> (std::move (this->vector->vector_set));
+        if (this->kdtree == nullptr && KD_THRESH(m, dim)) {
+          this->kdtree = std::make_unique<kdtree_backed<Vector>> (std::move (this->vector->vector_set));
           this->vector = nullptr;
           data_do (std::cout << "VEKD: upgraded to kd-tree downset" << std::endl);
         }
@@ -137,32 +137,32 @@ namespace downsets {
         // downset container; their respective constructors may reduce the
         // size by removing dominated elements... it's easier and clearer
         // to do the check here though
-        if (KD_THRESH(m, dim)) { 
-          this->kdtree = std::make_shared<kdtree_backed<Vector>> (std::move (elements));
+        if (KD_THRESH(m, dim)) {
+          this->kdtree = std::make_unique<kdtree_backed<Vector>> (std::move (elements));
           data_do (std::cout << "VEKD: created kd-tree downset" << std::endl);
         } else {
-          this->vector = std::make_shared<vector_backed<Vector>> (std::move (elements));
+          this->vector = std::make_unique<vector_backed<Vector>> (std::move (elements));
           data_do (std::cout << "VEKD: created vector downset" << std::endl);
         }
       }
 
       vector_or_kdtree_backed (Vector&& el) {
         // too small, just use a vector
-        this->vector = std::make_shared<vector_backed<Vector>> (std::move (el));
+        this->vector = std::make_unique<vector_backed<Vector>> (std::move (el));
       }
 
       template <typename F>
       auto apply (const F& lambda) const {
         const std::vector<Vector>& backing_vector =
           this->kdtree != nullptr ?
-          this->kdtree->tree->vector_set :
+          this->kdtree->tree.vector_set :
           this->vector->vector_set;
         std::vector<Vector> ss;
         ss.reserve (backing_vector.size ());
 
         for (const auto& v : backing_vector)
           ss.push_back (lambda (v));
-    
+
         return vector_or_kdtree_backed (std::move (ss));
       }
 
