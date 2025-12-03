5840cfd4e9007a1d904d58171bb0786070e0624f
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-03 16:53:38 +0100

    Moved game solver code to separate file.

--- src/composition/composition_mt.hh
+++ src/composition/composition_mt.hh
@@ -14,6 +14,7 @@
 #include "aut_preprocessors.hh"
 #include "k-bounded_safety_aut.hh"
 #include "create_safety_game.hh"
+#include "solve_game.hh"
 
 
 class composition_mt {
@@ -36,7 +37,7 @@ class composition_mt {
 
   spot::formula bdd_to_formula (bdd f) const; // for debugging
 
-  void solve_game (safety_game& game); // use the k-bounded safety aut to solve a game
+  // void solve_game (safety_game& game); // use the k-bounded safety aut to solve a game
   int epilogue (); // look at the final result and return whether it was realizable
 
   using aut_t = decltype (trans_.run (spot::formula::ff ()));
@@ -60,67 +61,6 @@ spot::formula composition_mt::bdd_to_formula (bdd f) const {
 }
 
 
-void composition_mt::solve_game (safety_game& game) {
-  spot::stopwatch sw;
-  sw.start ();
-
-  auto [nbitsetbools, actual_nonbools] = game.set_globals ();
-
-#define UNREACHABLE [] (int x) { assert (false); }
-
-  constexpr auto STATIC_ARRAY_CAP_MAX =
-    posets::vectors::traits<posets::vectors::ARRAY_IMPL, VECTOR_ELT_T>::capacity_for (STATIC_ARRAY_MAX);
-
-  if (actual_nonbools <= STATIC_ARRAY_CAP_MAX) { // Array & Bitsets
-    static_switch_t<STATIC_ARRAY_CAP_MAX> {} (
-    [&] (auto vnonbools) {
-      static_switch_t<STATIC_MAX_BITSETS> {} (
-      [&] (auto vbitsets) {
-        using SpecializedDownset = posets::downsets::ARRAY_AND_BITSET_DOWNSET_IMPL<
-          posets::vectors::x_and_bitset<
-            posets::vectors::ARRAY_IMPL<VECTOR_ELT_T, std::max (vnonbools.value, 1UL)>,
-            vbitsets.value>>;
-        auto skn = K_BOUNDED_SAFETY_AUT_IMPL<SpecializedDownset>
-        (game.aut, opt_Kmin, opt_K, opt_Kinc, all_inputs, all_outputs);
-        assert (game.safe);
-        auto current_safe = cast_downset<SpecializedDownset> (*game.safe);
-        auto safe = skn.solve (current_safe, invariant, init_state);
-        if (safe.has_value ()) {
-          game.safe = std::make_shared<GenericDownset> (cast_downset<GenericDownset> (safe.value ()));
-        } else game.safe = nullptr;
-      },
-      UNREACHABLE,
-      posets::vectors::nbools_to_nbitsets (nbitsetbools));
-    },
-    UNREACHABLE,
-    actual_nonbools);
-  }
-  else {                                  // Vectors & Bitsets
-    static_switch_t<STATIC_MAX_BITSETS> {} (
-    [&] (auto vbitsets) {
-      using SpecializedDownset = posets::downsets::VECTOR_AND_BITSET_DOWNSET_IMPL<
-      posets::vectors::x_and_bitset<
-      posets::vectors::VECTOR_IMPL<VECTOR_ELT_T>,
-      vbitsets.value>>;
-      auto skn = K_BOUNDED_SAFETY_AUT_IMPL<SpecializedDownset>
-      (game.aut, opt_Kmin, opt_K, opt_Kinc, all_inputs, all_outputs);
-      assert (game.safe);
-      auto current_safe = cast_downset<SpecializedDownset> (*game.safe);
-      auto safe = skn.solve (current_safe, invariant, init_state);
-      if (safe.has_value ()) {
-        game.safe = std::make_shared<GenericDownset> (cast_downset<GenericDownset> (safe.value ()));
-      } else game.safe = nullptr;
-    },
-    UNREACHABLE,
-    posets::vectors::nbools_to_nbitsets (nbitsetbools));
-  }
-
-  game.solved = true;
-  game.invariant = invariant;
-
-  double solve_time = sw.stop ();
-  verb_do (1, vout << "Safety game solved in " << solve_time << " seconds\n");
-}
 
 int composition_mt::epilogue () {
   // TODO: this should either return true (real) or false (unknown). This will require some changes.
@@ -148,7 +88,7 @@ int composition_mt::epilogue () {
   if ((!r.solved) || not_fully_solved) {
     if (!r.solved) verb_do (1, vout << "Not fully solved -> extra solve\n");
     if (not_fully_solved) verb_do (1, vout << "Solved but not with the right invariant -> extra solve\n");
-    solve_game (r);
+    solve_game (r, opt_K, opt_Kmin, opt_Kinc, all_inputs, all_outputs, init_state, invariant);
   }
   // if there is no safe region: return 0 (not winning)
   return r.safe != nullptr;
