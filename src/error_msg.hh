e73e0b7045e413e8bf9f2a94d6ce4e3d2f93f2f9
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-03 11:41:55 +0100

    Removed some conditional compilation.

--- src/error_msg.hh
+++ src/error_msg.hh
@@ -1,16 +1,13 @@
 #pragma once
 
-
-#if defined(__APPLE__)
-
-// macOS does not have the "error.h" header, so we use
-// the following function
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 
-inline void error(int status, int errnum, const char *format, ...) {
+// macOS does not have the "error.h" header, so we use
+// the following functions
+inline void error(int status, const char *format, ...) {
   va_list args;
   va_start(args, format);
   vfprintf(stderr, format, args);
@@ -19,26 +16,15 @@ inline void error(int status, int errnum, const char *format, ...) {
     exit(status);
 }
 
-inline void error_at_line(int status, int errnum,
-                                   const char *filename, unsigned int linenum,
-                                   const char *format, ...) {
+inline void error_at_line(int status, const char *filename, unsigned int linenum, const char *format, ...) {
   va_list args;
   fprintf(stderr, "%s:%u: ", filename, linenum);
   va_start(args, format);
   vfprintf(stderr, format, args);
   va_end(args);
-  if (errnum)
-    fprintf(stderr, ": %s", strerror(errnum));
   fputc('\n', stderr);
   if (status)
     exit(status);
 }
-
-
-#elif defined(__linux__)
-  #include <error.h>
-#else
-  #error "Unsupported platform"
-#endif
 
 
