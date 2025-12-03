63fb0d56873ec23ac595e2ea3d0d1f4e61dcd6e8
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-03 11:38:03 +0100

    Removed unused function.

--- src/composition/composition.hh
+++ src/composition/composition.hh
@@ -6,51 +6,6 @@
 
 #include "types.hh"
 
-// from https://spot.lre.epita.fr/tut21.html
-void custom_print (std::ostream& out, spot::twa_graph_ptr aut)
-{
-  // We need the dictionary to print the BDDs that label the edges
-  const spot::bdd_dict_ptr& dict = aut->get_dict();
-
-  // Some meta-data...
-  out << "Acceptance: " << aut->get_acceptance() << '\n';
-  out << "Number of sets: " << aut->num_sets() << '\n';
-  out << "Number of states: " << aut->num_states() << '\n';
-  out << "Number of edges: " << aut->num_edges() << '\n';
-  out << "Initial state: " << aut->get_init_state_number() << '\n';
-  out << "Atomic propositions:";
-  for (spot::formula ap: aut->ap())
-    out << ' ' << ap << " (=" << dict->varnum(ap) << ')';
-  out << '\n';
-
-  // Arbitrary data can be attached to automata, by giving them
-  // a type and a name.  The HOA parser and printer both use the
-  // "automaton-name" to name the automaton.
-  if (auto name = aut->get_named_prop<std::string>("automaton-name"))
-    out << "Name: " << *name << '\n';
-
-
-  // States are numbered from 0 to n-1
-  unsigned n = aut->num_states();
-  for (unsigned s = 0; s < n; ++s)
-  {
-    out << "State " << s << ":\n";
-
-    // The out(s) method returns a fake container that can be
-    // iterated over as if the contents was the edges going
-    // out of s.  Each of these edges is a quadruplet
-    // (src,dst,cond,acc).  Note that because this returns
-    // a reference, the edge can also be modified.
-    for (auto& t: aut->out(s))
-    {
-      out << "  edge(" << t.src << " -> " << t.dst << ")\n    label = ";
-      spot::bdd_print_formula(out, dict, t.cond);
-      out << "\n";
-      if (t.acc.count() > 0) out << "    acc sets = " << t.acc << '\n';
-    }
-  }
-}
-
 // spot::purge_unreachable_states calls the function set_rename2 which stores the vector we need in a global variable for further use
 std::vector<unsigned int> rename2;
 
