af05ea91cf09b40af937b3385f8074dced37218f
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-10 14:37:10 +0100

    Cleanup

--- src/solver/solver_invoker.cc
+++ src/solver/solver_invoker.cc
@@ -25,7 +25,7 @@ spot::formula parse_ltl_string(const std::string& input)
     auto pf = spot::parse_infix_psl
       (input, spot::default_environment::instance(), false, false);
 
-    if (!pf.f || !pf.errors.empty())
+    if ((not pf.f) or (not pf.errors.empty()))
     {
         pf.format_errors(std::cerr);
         error(EXIT_CODE_ERROR, "Error parsing LTL formula");
@@ -39,9 +39,9 @@ int run_ltl(spot::translator &trans,
                    std::vector<std::string> input_aps,
                    std::vector<std::string> output_aps,
                    spot::bdd_dict_ptr dict,
-                   unsigned opt_K,
-                   unsigned opt_Kmin,
-                   unsigned opt_Kinc,
+                   unsigned opt_k,
+                   unsigned opt_kmin,
+                   unsigned opt_kinc,
                    std::vector<int> init_state,
                    std::string formula) {
 
@@ -50,11 +50,11 @@ int run_ltl(spot::translator &trans,
     bdd all_outputs = bddtrue;
 
     for(std::string ap: input_aps) {
-        unsigned const v = dict->register_proposition (spot::formula::ap (ap), 0);
+        unsigned const v = dict->register_proposition (spot::formula::ap (ap), nullptr);
         all_inputs &= bdd_ithvar (v);
     }
     for(std::string ap: output_aps) {
-        unsigned const v = dict->register_proposition (spot::formula::ap (ap), 0);
+        unsigned const v = dict->register_proposition (spot::formula::ap (ap), nullptr);
         all_outputs &= bdd_ithvar (v);
     }
 
@@ -62,18 +62,18 @@ int run_ltl(spot::translator &trans,
     spot::formula spot_formula = parse_ltl_string(formula);
 
     auto aut = create_automaton(std::move(spot_formula), trans);
-    aut_preprocessors::standard::make (aut, all_inputs, all_outputs, opt_K) ();
+    aut_preprocessors::standard::make (aut, all_inputs, all_outputs, opt_k) ();
     // aut_preprocessors::surely_losing::make (aut, all_inputs, all_outputs, K) ();
 
-    posets::vectors::bool_threshold = (boolean_states::forward_saturation::make (aut, opt_K)) ();
+    posets::vectors::bool_threshold = (boolean_states::forward_saturation::make (aut, opt_k)) ();
     // posets::vectors::bool_threshold = (boolean_states::no_boolean_states::make (aut, opt_K)) ();
 
 
-    safety_game game{aut, opt_Kmin, posets::vectors::bool_threshold};
+    safety_game game{aut, opt_kmin, posets::vectors::bool_threshold};
 
-    int res = solve_game (game, opt_K, opt_Kmin, opt_Kinc, all_inputs, all_outputs, std::move(init_state), bddtrue);
+    bool const res = solve_game (game, opt_k, opt_kmin, opt_kinc, all_inputs, all_outputs, std::move(init_state), bddtrue);
 
-    dict->unregister_all_my_variables (0);
+    dict->unregister_all_my_variables (nullptr);
 
     return res;
 }
