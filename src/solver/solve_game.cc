5968e80ead70d5e7b5e90d79fd88a8aa76dd3ba7
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-17 15:44:49 +0100

    Refactor WIP

--- src/solver/solve_game.cc
+++ src/solver/solve_game.cc
@@ -37,7 +37,7 @@ bool solve_game (safety_game& game, unsigned kmax, unsigned kmin, unsigned kinc,
                     posets::downsets::ARRAY_AND_BITSET_DOWNSET_IMPL<posets::vectors::x_and_bitset<
                         posets::vectors::ARRAY_IMPL<VECTOR_ELT_T, std::max (vnonbools.value, 1UL)>,
                         vbitsets.value>>;
-                using SetOfStates = SpecializedDownset; // This is how the ACTIONER directive is defined!
+                // using SetOfStates = SpecializedDownset; // This is how the ACTIONER directive is defined!
                 // TODO: why does this template not work?
                 // auto skn = k_bounded_safety_aut<SpecializedDownset> (
                     // game.aut, kmin, kmax, kinc, all_inputs, all_outputs, IOS_PRECOMPUTER (), ACTIONER<typename SetOfStates::value_type> (), INPUT_PICKER ());
@@ -62,7 +62,7 @@ bool solve_game (safety_game& game, unsigned kmax, unsigned kmin, unsigned kinc,
           using SpecializedDownset =
               posets::downsets::VECTOR_AND_BITSET_DOWNSET_IMPL<posets::vectors::x_and_bitset<
                   posets::vectors::VECTOR_IMPL<VECTOR_ELT_T>, vbitsets.value>>;
-          using SetOfStates = SpecializedDownset;  // This is how the ACTIONER directive is defined!
+          // using SetOfStates = SpecializedDownset;  // This is how the ACTIONER directive is defined!
           // auto skn = k_bounded_safety_aut<SpecializedDownset> (game.aut, kmin, kmax, kinc,
           // all_inputs, all_outputs, IOS_PRECOMPUTER (), ACTIONER<typename SetOfStates::value_type> (), INPUT_PICKER ());
           auto skn = K_BOUNDED_SAFETY_AUT_IMPL<SpecializedDownset> (game.aut, kmin, kmax, kinc, all_inputs, all_outputs);
