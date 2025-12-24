1755f3da94a58924ff1e329d540fb25d9c6ee667
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-24 17:57:48 +0100

    Added some function names for the different steps of the solver.

---
+++ src/python/python_interface.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+
+void parse_formula();
+
+// needed for UNREAL_X_FORMULA
+void add_x_to_outputs();
+
+void create_bdds();
+
+void create_automaton();
+
+void push_outputs();
+
+void preprocess_aut();
+
+
+void set_bool_thresh();
+
+void solve_game();
+
+
