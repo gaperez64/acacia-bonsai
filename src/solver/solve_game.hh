6762c8b2d6a5f1d4257fee5dc421873121fb8c58
Kasper Engelen <kasper.engelen@hotmail.com>
2025-12-25 17:06:08 +0100

    Some template thing I cannot wrap my head around...

--- src/solver/solve_game.hh
+++ src/solver/solve_game.hh
@@ -106,3 +106,67 @@ bool solve_game (spot::twa_graph_ptr aut, VECTOR_ELT_T kmax, VECTOR_ELT_T kmin, 
 
   return realizable;
 }
+
+bool get_winning_region(spot::twa_graph_ptr aut, VECTOR_ELT_T kmax, VECTOR_ELT_T kmin, VECTOR_ELT_T kinc,
+                 bdd all_inputs, bdd all_outputs) {
+  // Compute how many boolean states will actually be put in bitsets.
+  constexpr auto max_bools_in_bitsets = posets::vectors::nbitsets_to_nbools (STATIC_MAX_BITSETS);
+  auto nbitsetbools = aut->num_states () - posets::vectors::bool_threshold;
+  if (nbitsetbools > max_bools_in_bitsets) {
+    verb_do (1, vout << "Warning: bitsets not large enough, using regular vectors for some "
+                        "Boolean states.\n"
+                     /*   */
+                     << "\tTotal # of Boolean-for-bitset states: "
+                     << nbitsetbools
+                     /*   */
+                     << ", max: " << max_bools_in_bitsets << std::endl);
+    nbitsetbools = max_bools_in_bitsets;
+  }
+
+#ifdef NO_ARRAY_CAP_MAX
+# pragma message("STATIC_ARRAY_CAP_MAX is being set to 0!")
+  constexpr auto STATIC_ARRAY_CAP_MAX = 0;
+#else
+  constexpr auto STATIC_ARRAY_CAP_MAX =
+      posets::vectors::traits<posets::vectors::ARRAY_IMPL, VECTOR_ELT_T>::capacity_for (
+          STATIC_ARRAY_MAX);
+#endif
+
+  // Maximize usage of the nonbool implementation
+  auto nonbools = aut->num_states () - nbitsetbools;
+  size_t actual_nonbools =
+      (nonbools <= STATIC_ARRAY_CAP_MAX)
+          ? posets::vectors::traits<posets::vectors::ARRAY_IMPL, VECTOR_ELT_T>::capacity_for (
+                nonbools)
+          : posets::vectors::traits<posets::vectors::VECTOR_IMPL, VECTOR_ELT_T>::capacity_for (
+                nonbools);
+  if (actual_nonbools >= aut->num_states ())
+    nbitsetbools = 0;
+  else
+    nbitsetbools -= (actual_nonbools - nonbools);
+
+  posets::vectors::bitset_threshold = aut->num_states () - nbitsetbools;
+
+  verb_do (1, vout << "Bitset threshold set at " << posets::vectors::bitset_threshold << "\n");
+
+  bool realizable = false;
+
+
+  static_switch_t<STATIC_MAX_BITSETS> {}(
+      [&] (auto vbitsets) {
+        using SpecializedDownset =
+            posets::downsets::VECTOR_AND_BITSET_DOWNSET_IMPL<posets::vectors::x_and_bitset<
+                posets::vectors::VECTOR_IMPL<VECTOR_ELT_T>, vbitsets.value>>;
+
+        using IOsPrecomputationMaker = IOS_PRECOMPUTER;
+        using ActionerMaker = ACTIONER<typename SpecializedDownset::value_type>;
+        using InputPickerMaker = INPUT_PICKER;
+        auto skn = k_bounded_safety_aut_detail<SpecializedDownset, IOsPrecomputationMaker,
+                                               ActionerMaker, InputPickerMaker> (
+            aut, kmin, kmax, kinc, all_inputs, all_outputs, IOS_PRECOMPUTER (),
+            ACTIONER<typename SpecializedDownset::value_type> (), INPUT_PICKER ());
+        // TODO: how to return this value?
+        realizable = skn.solve ().has_value ();
+      },
+      UNREACHABLE, posets::vectors::nbools_to_nbitsets (nbitsetbools));
+}
