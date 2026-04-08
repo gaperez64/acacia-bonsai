5d417cc1acf3e3f5d70942b254c7b4823a4fc081
Kasper Engelen <kasper.engelen@hotmail.com>
2026-04-08 11:00:51 +0200

    Python example.

--- python_examples/examples.py
+++ python_examples/examples.py
@@ -13,3 +13,10 @@ acacia_python.preprocess_aut_standard(twa, ios2, k_max=99)
 acacia_python.set_bool_thresh_no_bool_states(twa, k_max=99)
 result = acacia_python.solve_safety_game(twa, ios2, k_max=99, k_min=2, k_inc=3)
 
+region = acacia_python.get_winning_region_of_game(twa, ios2, k_max=99, k_min=2, k_inc=3)
+
+for vec in region:
+    print("---")
+    for elem in vec:
+        print(elem)
+
