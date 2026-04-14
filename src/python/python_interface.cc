d8171f43e38d2a7135d4b71bc95060ea86542ecb
Kasper Engelen <kasper.engelen@hotmail.com>
2026-04-14 16:47:56 +0200

    Python interface is no longer correct after merge. Added a few TODOs with what needs to be changed.

--- src/python/python_interface.cc
+++ src/python/python_interface.cc
@@ -4,8 +4,9 @@
 #include "python_interface.hh"
 #include "solver/solver_invoker.hh"
 
-// TODO: this is to get the verbose debug printing working, needs to refactored out.
+
 utils::voutstream utils::vout;
+// TODO: this no longer works, why?
 int               utils::verbose = 0;
 
 // TODO We need to figure out some clean way to define these, instead of copying them everywhere
@@ -18,6 +19,7 @@ io_spec get_io_spec(const std::vector<std::string>& input_aps, const std::vector
 }
 
 void prep_unreal_formula (spot::formula& formula, std::vector<std::string>& output_aps) {
+  // TODO: this no longer works
   add_x_to_outputs (formula, output_aps);
 }
 
@@ -56,6 +58,7 @@ spot::twa_graph_ptr create_twa (spot::formula& formula, bdd_io_spec& io_spec) {
 }
 
 void prep_unreal_automaton (spot::twa_graph_ptr twa, bdd_io_spec& io_spec) {
+  // TODO: this no longer works
   push_outputs (twa, io_spec.inputs, io_spec.outputs);
 }
 
@@ -77,13 +80,18 @@ void set_bool_thresh_forward_saturation (spot::twa_graph_ptr twa, int k_max) {
   posets::vectors::bool_threshold = boolean_states::forward_saturation::make (twa, k_max)();
 }
 
+// TODO: these two functions need to be merged into one function "solve_game". This
+//  will return a wrapper around a "game result". This then (a) tells you REAL/UNREAL
+//  and (b) returns an iterable.
 bool solve_acacia_safety_game (spot::twa_graph_ptr twa, bdd_io_spec& io_spec, int k_max, int k_min, int k_inc) {
+  // TODO: this no longer works
   bool res = solve_game (twa, k_max, k_min, k_inc, io_spec.inputs, io_spec.outputs);
   return res;
 }
 
 const winreg_iterator* get_winning_region_of_game (spot::twa_graph_ptr twa, bdd_io_spec& io_spec,
                                                    int k_max, int k_min, int k_inc) {
+  // TODO: this no longer works
   auto winning_region = get_winning_region (twa, k_max, k_min, k_inc, io_spec.inputs, io_spec.outputs);
 
   if (winning_region.has_value ()) {
