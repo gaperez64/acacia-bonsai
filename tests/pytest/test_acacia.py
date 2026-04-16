7af7cf71bf7f61a88bd0cab53e146b0508175619
Kasper Engelen <kasper.engelen@hotmail.com>
2026-04-16 18:52:20 +0200

    Minimal pytest example.

---
+++ tests/pytest/test_acacia.py
@@ -0,0 +1,40 @@
+
+import spot
+import acacia_python
+
+def _check_real_():
+
+    s = "!((G (F (req))) -> (G (F (grant))))"
+    inputs = ["req"]
+    outputs = ["grant"]
+    twa = acacia_python.create_twa(s, inputs, outputs)
+
+    print("Automaton (HOA):")
+    aut = spot.automaton(acacia_python.get_aut_hoa(twa))
+    print(aut.to_str('hoa'))
+
+    acacia_python.preprocess_aut_standard(twa, k_max=99)
+    # acacia_python.preprocess_aut_surely_losing(twa, k_max=99)
+    acacia_python.set_bool_thresh_no_bool_states(twa, k_max=99)
+    # acacia_python.set_bool_thresh_forward_saturation(twa, k_max=99)
+
+    print("Solving safety game...")
+    # type(game_result) = GameResult
+    game_result = acacia_python.solve_acacia_safety_game(twa, k_max=99, k_min=2, k_inc=3)
+    print("Done.")
+    print("Solve result:", game_result.is_real())
+
+    # type(winning_region) = Optional[WinningRegion]
+    winning_region = game_result.get_winning_region()
+
+    print("Does the game result contain the initial state?")
+    contains = winning_region.contains(acacia_python.get_initial_state(twa))
+    return contains
+
+
+
+def test_string_equality():
+
+    assert "hello" == "hello"
+    assert _check_real_()
+
