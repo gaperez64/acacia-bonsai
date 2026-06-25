df0f4e4ac7b38433011d681ff8b8728e2db292cf
gaperez64 <gaperez64@gmail.com>
2026-06-25 23:58:34 +0200

    Exercise GFG and Elevator configs in CI

--- src/aut_preprocessors.hh
+++ src/aut_preprocessors.hh
@@ -3,8 +3,5 @@
 #include "aut_preprocessors/no_preprocessing.hh"
 #include "aut_preprocessors/standard.hh"
 #include "aut_preprocessors/surely_losing.hh"
-
-#ifdef ENABLE_ELEVATOR_PREPROCESSOR
-# include "aut_preprocessors/elevator.hh"
-#endif
+#include "aut_preprocessors/elevator.hh"
 #include "configuration.hh"
